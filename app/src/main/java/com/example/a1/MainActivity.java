2e47945188853af1260ac577edbe656bf10b5658
manjukunda <manjunath.k@lngtechnologies.in>
2020-09-15 18:21:37 +0530

    a

---
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+public class MainActivity extends AppCompatActivity {
+    YouTubePlayer Player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+       // getLifecycle().addObserver(youTubePlayerView);
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "S0Q4gqBUs7c";
+                youTubePlayer.loadVideo(videoId, 0);
+                Player=youTubePlayer;
+                Player.pause();
+            }
+        });
+    }
+
+    public void play(View view) {
+
+        Player.loadVideo("sf7VoyW_5ro", 0);
+        Player.play();
+
+    }
+}
+
